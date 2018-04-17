1e6b9df55d03417be6306bbbeb8db793ff23e73c
Paul Henschel <drcmda@gmail.com>
2018-04-17 23:44:38 +0200

    rgba in normalize-css-color isn't needed

--- src/normalize-css-color/index.js
+++ src/normalize-css-color/index.js
@@ -321,13 +321,4 @@ var names = {
   yellowgreen: 0x9acd32ff,
 }
 
-function rgba(colorInt) {
-  var r = Math.round((colorInt & 0xff000000) >>> 24)
-  var g = Math.round((colorInt & 0x00ff0000) >>> 16)
-  var b = Math.round((colorInt & 0x0000ff00) >>> 8)
-  var a = ((colorInt & 0x000000ff) >>> 0) / 255
-  return { r, g, b, a }
-}
-
-normalizeColor.rgba = rgba
 normalizeColor.colorNames = names
