1315c02d79351d879a7bb9efca824c61af4e1e9f
Paul Henschel <drcmda@gmail.com>
2018-04-18 17:52:53 +0200

    cleanup rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,8 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import uglify from 'rollup-plugin-uglify'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
-import pkg from './package.json'
 
+const isExternal = id => !id.startsWith('.') && !id.startsWith('/')
 const getBabelOptions = ({ useESModules }) => ({
   babelrc: false,
   exclude: '**/node_modules/**',
@@ -23,106 +23,51 @@ const getBabelOptions = ({ useESModules }) => ({
   ],
 })
 
-const isExternal = id => !id.startsWith('.') && !id.startsWith('/')
-
-const plugins = [
-  babel(getBabelOptions({ useESModules: false })),
-  resolve(),
-  commonjs(),
-  sizeSnapshot(),
-  uglify({
-    compress: true,
-    mangle: {
-      toplevel: true,
+function createConfig(entry, out, name) {
+  return [
+    {
+      input: `./src/${entry}.js`,
+      output: { file: `dist/${out}.es.js`, format: 'es' },
+      external: isExternal,
+      plugins: [babel(getBabelOptions({ useESModules: true })), sizeSnapshot()],
     },
-  }),
-]
-
-const globals = {
-  react: 'React',
-  'react-dom': 'ReactDOM',
-  'prop-types': 'PropTypes',
-  'react-spring': 'ReactSpring',
+    {
+      input: `./src/${entry}.js`,
+      output: { file: `dist/${out}.cjs.js`, format: 'cjs' },
+      external: isExternal,
+      plugins: [babel(getBabelOptions({ useESModules: false }))],
+    },
+    {
+      input: `./src/${entry}.js`,
+      output: {
+        file: `dist/${out}.umd.js`,
+        format: 'umd',
+        name,
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'prop-types': 'PropTypes',
+          'react-spring': 'ReactSpring',
+        },
+      },
+      external: ['react', 'react-dom', 'prop-types', 'react-spring'],
+      plugins: [
+        babel(getBabelOptions({ useESModules: false })),
+        resolve(),
+        commonjs(),
+        sizeSnapshot(),
+        uglify({ compress: true, mangle: { toplevel: true } }),
+      ],
+    },
+  ]
 }
 
-const external = ['react', 'react-dom', 'prop-types', 'react-spring']
-
 export default [
-  {
-    input: './src/index.js',
-    output: { file: `${pkg.main}.js`, format: 'cjs' },
-    external: isExternal,
-    plugins: [babel(getBabelOptions({ useESModules: false }))],
-  },
-
-  {
-    input: './src/index.js',
-    output: { file: `${pkg.module}.js`, format: 'es' },
-    external: isExternal,
-    plugins: [babel(getBabelOptions({ useESModules: true })), sizeSnapshot()],
-  },
-
-  {
-    input: './src/index.js',
-    output: {
-      file: `${pkg.main}.umd.js`,
-      format: 'umd',
-      name: 'ReactSpring',
-      globals,
-    },
-    external,
-    plugins,
-  },
-
-  {
-    input: './src/addons/NumericalSpring.js',
-    output: { file: 'dist/NumericalSpring.cjs.js', format: 'cjs' },
-    external: isExternal,
-    plugins: [babel(getBabelOptions({ useESModules: false })), sizeSnapshot()],
-  },
-
-  {
-    input: './src/addons/NumericalSpring.js',
-    output: { file: 'dist/NumericalSpring.js', format: 'es' },
-    external: isExternal,
-    plugins: [babel(getBabelOptions({ useESModules: true })), sizeSnapshot()],
-  },
-
-  {
-    input: './src/addons/NumericalSpring.js',
-    output: {
-      file: 'dist/NumericalSpring.umd.js',
-      format: 'umd',
-      name: 'ReactSpringNumerical',
-      globals,
-    },
-    external,
-    plugins,
-  },
-
-  {
-    input: './src/addons/index.js',
-    output: { file: `dist/addons.js`, format: 'es' },
-    external: isExternal,
-    plugins: [babel(getBabelOptions({ useESModules: true })), sizeSnapshot()],
-  },
-
-  {
-    input: './src/addons/index.js',
-    output: { file: `dist/addons.cjs.js`, format: 'cjs' },
-    external: isExternal,
-    plugins: [babel(getBabelOptions({ useESModules: false }))],
-  },
-
-  {
-    input: './src/addons/index.js',
-    output: {
-      file: `dist/addons.umd.js`,
-      format: 'umd',
-      name: 'ReactSpringAddons',
-      globals,
-    },
-    external,
-    plugins,
-  },
+  ...createConfig('index', 'react-spring', 'ReactSpring'),
+  ...createConfig(
+    'addons/NumericalSpring',
+    'react-spring-numerical',
+    'ReactSpringNumerical'
+  ),
+  ...createConfig('addons/index', 'addons', 'ReactSpringAddons'),
 ]
