082c67f208b9009103dded683809171e7501e7da
a-type <gforrest@bandwidth.com>
2018-05-04 11:42:51 -0400

    Revert visibility trick; doesn't work for all cases

--- src/animated/targets/react-dom/fix-auto.js
+++ src/animated/targets/react-dom/fix-auto.js
@@ -29,7 +29,6 @@ export default function fixAuto(spring, props) {
 
   return (
     <div
-      style={{ visibility: 'hidden', position: 'absolute' }}
       ref={ref => {
         if (ref) {
           // Once it's rendered out, fetch bounds
