7d5bd90339c077a7a150d861d07138fad7ec9b04
Paul Henschel <drcmda@gmail.com>
2018-06-16 16:28:25 +0200

    prepare testing env

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,17 +1,4 @@
 import Enzyme, { mount } from 'enzyme'
-import Adapter from './jest.react.16.adapter'
-import delay from 'delay'
+import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
-
-global.snapshot = async function(Root, mutation) {
-  const tree = mount(Root)
-  expect(tree).toMatchSnapshot()
-  if (mutation) {
-    await mutation(tree)
-    await delay(25)
-    tree.update()
-    expect(tree).toMatchSnapshot()
-  }
-  tree.unmount()
-}
--- tests/test.js
+++ tests/test.js
@@ -1,39 +1,30 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { mount } from 'enzyme'
-import { Spring } from './../src/targets/web/index'
+import { Spring, animated } from './../src/targets/web/index'
 
-it('basic spring', async () => {
-  return new Promise(resolve => {
-    const from = {
-      opacity: 0,
-      width: '0%',
-      transform: 'scale(0) translate3d(0px,0,0)',
-      color: 'red',
-    }
-
-    const to = {
-      opacity: 1,
-      pointerEvents: 'all',
-      width: '100%',
-      transform: 'scale(1) translate3d(10px,0,0)',
-      color: '#aabbcc',
-    }
-
-    const tree = mount(
-      <Spring from={from} to={to} onRest={() => requestAnimationFrame(onRest)}>
-        {styles => <div style={styles} />}
+const testSpring = (props, result) =>
+  new Promise(resolve => {
+    let tree = mount(
+      <Spring
+        {...props}
+        onRest={() =>
+          process.nextTick(() => {
+            tree = tree.update()
+            result = result || props.to
+            expect(tree.find('div').get(0).props.style).toMatchObject(result)
+            tree.unmount()
+            resolve()
+          })
+        }>
+        {props.native
+          ? styles => <animated.div style={styles} />
+          : styles => <div style={styles} />}
       </Spring>
     )
-
-    function onRest() {
-      tree.update()
-      //console.log(tree.find('div').get(0).props.style)
-      expect(tree.find('div').get(0).props.style).toMatchObject({
-        ...to,
-        color: 'rgba(170, 187, 204, 1)',
-      })
-      tree.unmount()
-      resolve()
-    }
   })
+
+test('numbers', async () => {
+  await testSpring({ from: { opacity: 0 }, to: { opacity: 1 } })
+  //await testSpring({ native: true, from: { opacity: 0 }, to: { opacity: 1 } })
 })
