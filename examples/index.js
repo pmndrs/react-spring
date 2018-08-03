340cbc1f58e4726ec98b8a027b00e66accce04a2
Paul Henschel <drcmda@gmail.com>
2018-08-03 21:07:57 +0200

    this keeps happening ...

--- examples/index.js
+++ examples/index.js
@@ -1,38 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Transition, animated } from 'react-spring'
-
+import Loadable from 'react-loadable'
 import './styles.css'
 
-function Sub() {
-  return <div style={{ backgroundColor: 'pink', height: 100 }}>Hey</div>
-}
+const components = [
+  'scroll',
+  'parallax',
+  'nativespring',
+  'transitions',
+  'areas',
+  'trails',
+  'reveals',
+  'timing',
+  'sunburst',
+  'gestures',
+  'tree',
+  'morph',
+].map(path =>
+  Loadable({
+    loader: () => import('./demos/' + path),
+    loading: () => <div />,
+  })
+)
 
-class App extends React.Component {
-  state = { toggle: false }
-  handleClick = () => {
-    this.setState({ toggle: !this.state.toggle })
-  }
-  render() {
-    return (
-      <div className="App">
-        {this.state.toggle ? <div>true</div> : <div>false</div>}
-        <button onClick={this.handleClick}>button</button>
-        <Transition
-          from={{ opacity: 0, height: 0 }}
-          enter={{ opacity: 1, height: 'auto' }}
-          leave={{ opacity: 0 }}>
-          {this.state.toggle &&
-            (style => (
-              <animated.div style={{ ...style, background: 'orange' }}>
-                <Sub />
-              </animated.div>
-            ))}
-        </Transition>
-      </div>
-    )
-  }
-}
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(
+  <div className="app-container">
+    {components.map((Component, i) => <Component key={i} />)}
+  </div>,
+  document.getElementById('root')
+)
