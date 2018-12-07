07f780a67f8d6937f8c01e07f829e7ead3f04180
Joachim Westphal <elindorath@gmail.com>
2018-12-08 00:17:44 +0100

    create and inject AnimatedReactNativeStyle

--- src/targets/native/index.js
+++ src/targets/native/index.js
@@ -3,6 +3,7 @@ import Controller from '../../animated/Controller'
 import { interpolate } from '../../animated/AnimatedInterpolation'
 import animated from '../../animated/createAnimatedComponent'
 import { config } from '../../shared/constants'
+import AnimatedStyle from '../../animated/AnimatedStyle'
 import Spring from '../../Spring'
 import Transition from '../../Transition'
 import Trail from '../../Trail'
@@ -10,6 +11,7 @@ import Keyframes from '../../Keyframes'
 import createInterpolation from '../../shared/interpolation'
 import colorNames from '../../shared/colors'
 import AnimatedTransform from './AnimatedTransform'
+import AnimatedReactNativeStyle from './AnimatedReactNativeStyle'
 import { View } from 'react-native'
 
 Globals.injectDefaultElement(View)
@@ -19,6 +21,9 @@ Globals.injectApplyAnimatedValues(
   (instance, props) =>
     instance.setNativeProps ? instance.setNativeProps(props) : false,
   style => ({ ...style, transform: new AnimatedTransform(style.transform) })
+)
+Globals.injectCreateAnimatedStyle(
+  (styles) => Array.isArray(styles) ? new AnimatedReactNativeStyle(styles) : new AnimatedStyle(styles)
 )
 
 export {
