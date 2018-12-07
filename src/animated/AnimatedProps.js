f231ea277398fbfb6a28a83368df0a5080570fbc
Joachim Westphal <elindorath@gmail.com>
2018-12-08 00:09:53 +0100

    use createAnimatedStyle in AnimatedProps

--- src/animated/AnimatedProps.js
+++ src/animated/AnimatedProps.js
@@ -2,11 +2,12 @@ import Animated from './Animated'
 import AnimatedStyle from './AnimatedStyle'
 import { AnimatedObjectWithChildren } from './AnimatedWithChildren'
 import { getValues } from '../shared/helpers'
+import * as Globals from './Globals'
 
 export default class AnimatedProps extends AnimatedObjectWithChildren {
   constructor(props, callback) {
     super()
-    if (props.style) props = { ...props, style: new AnimatedStyle(props.style) }
+    if (props.style) props = { ...props, style: Globals.createAnimatedStyle(props.style) }
     this.payload = props
     this.update = callback
     this.attach()
