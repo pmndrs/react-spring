91027c4a56b7eb3e5829fb14a12c615eb356b958
Paul Henschel <drcmda@gmail.com>
2018-12-14 00:12:30 +0100

    Update Partition.js

--- examples/demos/sunburst/Partition.js
+++ examples/demos/sunburst/Partition.js
@@ -2,7 +2,6 @@ import React from 'react'
 import cx from 'classnames'
 import { Group } from '@vx/group'
 import { partition as d3partition } from 'd3-hierarchy'
-import { HierarchyDefaultNode as DefaultNode } from '@vx/hierarchy'
 
 export default function Partition({
   top,
@@ -13,18 +12,13 @@ export default function Partition({
   round,
   padding,
   children,
-  nodeComponent = DefaultNode,
   ...restProps
 }) {
   const partition = d3partition()
   if (size) partition.size(size)
   if (round) partition.round(round)
   if (padding) partition.padding(padding)
-
-  const data = partition(root)
-  const links = data.links()
-  const descendants = root.descendants()
-
+  partition(root)
   return (
     <Group top={top} left={left} className={cx('vx-partition', className)}>
       {children}
