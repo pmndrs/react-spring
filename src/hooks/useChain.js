0d0ed24c1fe3bc554fc787939df11846597d8fa4
Paul Henschel <drcmda@gmail.com>
2018-12-17 16:30:35 +0100

    fix useChain typo

--- src/hooks/useChain.js
+++ src/hooks/useChain.js
@@ -1,17 +1,10 @@
 import React, { useEffect } from 'react'
 
-// TODO, something needs to prevent left-over async calls when useChain has been called while
-// we're in the middle of a chain. Guid === Local should do this, but something is not yet right
-
-// let guid = 0
 export function useChain(args) {
-  // const local = ++guid
   useEffect(() => {
     args.reduce(
-      (q, ({ current })) =>
-        (q = q.then(
-          () => current && /*guid === local &&*/ new Promise(current.start)
-        )),
+      (q, { current }) =>
+        (q = q.then(() => current && new Promise(current.start))),
       Promise.resolve()
     )
   }, args)
