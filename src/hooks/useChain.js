0ac7dd5b041639c286a97b817448dd90b90b327e
Paul Henschel <drcmda@gmail.com>
2018-12-17 10:11:09 +0100

    shorten useChain, guid===local draft

--- src/hooks/useChain.js
+++ src/hooks/useChain.js
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react'
 
+// TODO, something needs to prevent left-over async calls when useChain has been called while
+// we're in the middle of a chain. Guid === Local should do this, but something is not yet right
+
+// let guid = 0
 export function useChain(args) {
+  // const local = ++guid
   useEffect(() => {
-    let queue = Promise.resolve()
-    for (let ref of args) {
-      if (ref && ref.current)
-        queue = queue.then(r => new Promise(ref.current.start))
-    }
+    args.reduce(
+      (q, r) =>
+        (q = q.then(
+          () => r.current && /*guid === local &&*/ new Promise(r.current.start)
+        )),
+      Promise.resolve()
+    )
   }, args)
 }
