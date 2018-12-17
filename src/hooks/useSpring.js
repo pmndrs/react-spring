a121c5ae15b2939ba537d0124234ba69952bdf70
Paul Henschel <drcmda@gmail.com>
2018-12-17 17:53:10 +0100

    add descriptions

--- src/hooks/useSpring.js
+++ src/hooks/useSpring.js
@@ -9,11 +9,14 @@ import Controller from '../animated/Controller'
 import { requestFrame } from '../animated/Globals'
 
 export function useSpring(args) {
+  const [, forceUpdate] = useState()
+  // Extract animation props and hook-specific props, can be a function or an obj
   const isFn = typeof args === 'function'
   const { onRest, onKeyframesHalt, ...props } = isFn ? args() : args
+  // The controller maintains the animation values, starts and tops animations
   const [ctrl] = useState(() => new Controller(props))
+  // Define onEnd callbacks and resolvers
   const endResolver = useRef(null)
-  const [, forceUpdate] = useState()
   const onHalt = onKeyframesHalt
     ? onKeyframesHalt(ctrl)
     : ({ finished }) => {
@@ -23,24 +26,25 @@ export function useSpring(args) {
         }
       }
 
+  // The hooks explcit API gets defined here ...
   useImperativeMethods(props.ref, () => ({
     start: resolve =>
       void ((endResolver.current = resolve), ctrl.start(onHalt)),
   }))
 
+  // Defines the hooks setter, which updates the controller
   const updateCtrl = useCallback(
     updateProps => {
       ctrl.update(updateProps)
-      if (!props.ref) {
-        endResolver.current = null
-        ctrl.start(onHalt)
-      }
+      if (!props.ref) ctrl.start(onHalt)
       if (props.reset) requestFrame(forceUpdate)
     },
     [onRest, onKeyframesHalt, props.ref]
   )
-
+  
+  // Update next frame is props aren't functional
   useEffect(() => void (!isFn && updateCtrl(props)))
+  // Return animated props, or, anim-props + the update-setter above
   const propValues = ctrl.getValues()
   return isFn
     ? [propValues, updateCtrl, (finished = false) => ctrl.stop(finished)]
