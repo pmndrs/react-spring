c7e924b0b39d5f6fb3274f3e4b529852db86f5ff
Paul Henschel <drcmda@gmail.com>
2019-02-20 16:23:06 -0500

    web/globals missing

--- src/targets/konva/index.ts
+++ src/targets/konva/index.ts
@@ -14,6 +14,7 @@ import { useSprings } from '../../useSprings'
 import { useTrail } from '../../useTrail'
 import { useTransition } from '../../useTransition'
 import { Spring, Trail, Transition } from '../../classes'
+import './globals'
 
 Globals.injectDefaultElement('Group')
 Globals.injectInterpolation(createInterpolation)
