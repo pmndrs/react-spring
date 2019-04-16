c59030927cd552a19d505d75b651f7aa3d938ff3
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-16 10:28:09 -0400

    fix: "useChain" bug
    - Remove the comparison between `refs` and the previous refs
    - Use the previous timeStep if none exists
    - When no timeSteps are given, remove all updates from each controller queue until its previous controller finishes

--- src/useChain.js
+++ src/useChain.js
@@ -1,33 +1,51 @@
-import { useEffect, useMemo, useRef } from 'react'
-import { is } from './shared/helpers'
+import { useEffect } from 'react'
 
 /** API
  *  useChain(references, timeSteps, timeFrame)
  */
 
 export function useChain(refs, timeSteps, timeFrame = 1000) {
-  const previous = useRef()
   useEffect(() => {
-    if (is.equ(refs, previous.current))
-      refs.forEach(({ current }) => current && current.start())
-    else if (timeSteps) {
-      refs.forEach(({ current }, index) => {
-        if (current) {
-          const ctrls = current.controllers
-          if (ctrls.length) {
-            const t = timeFrame * timeSteps[index]
-            ctrls.forEach(ctrl => {
-              ctrl.queue.forEach(props => (props.delay += t))
-              ctrl.start()
-            })
-          }
+    if (timeSteps) {
+      let prevDelay = 0
+      refs.forEach((ref, i) => {
+        if (!ref.current) return
+
+        const { controllers } = ref.current
+        if (controllers.length) {
+          let delay = timeFrame * timeSteps[i]
+
+          // Use the previous delay if none exists.
+          if (isNaN(delay)) delay = prevDelay
+          else prevDelay = delay
+
+          controllers.forEach(ctrl => {
+            ctrl.queue.forEach(props => (props.delay += delay))
+            ctrl.start()
+          })
         }
       })
-    } else
-      refs.reduce(
-        (q, { current }, rI) => (q = q.then(() => current.start())),
-        Promise.resolve()
-      )
-    previous.current = refs
+    } else {
+      let p = Promise.resolve()
+      refs.forEach(ref => {
+        if (!ref.current) return
+
+        const { controllers, start } = ref.current
+        if (controllers.length) {
+          // Take the queue of each controller
+          const updates = controllers.map(ctrl => {
+            const q = ctrl.queue
+            ctrl.queue = []
+            return q
+          })
+
+          // Apply the queue when the previous ref stops animating
+          p = p.then(() => {
+            controllers.forEach((ctrl, i) => ctrl.queue.push(...updates[i]))
+            return start()
+          })
+        }
+      })
+    }
   })
 }
