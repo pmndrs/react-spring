46ea452bd5a86a3fd6e62641dfd57b88504bf834
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-24 08:28:30 -0400

    fix(ts): various type fixes

--- types/web.d.ts
+++ types/web.d.ts
@@ -1,17 +1,10 @@
+import { AnimatedStyle, AnimatedValue, Solve } from './lib/common'
 import {
-  animated as createAnimatedComponent,
-  AnimatedComponent,
-} from './lib/common'
-
-export const animated: typeof createAnimatedComponent &
-  { [Tag in keyof JSX.IntrinsicElements]: AnimatedComponent<Tag> }
-
-export {
-  config,
-  AnimatedValue,
-  SpringConfig,
-  TransitionPhase,
-} from './lib/common'
+  ReactType,
+  CSSProperties,
+  ComponentPropsWithRef,
+  ForwardRefExoticComponent,
+} from 'react'
 
 export * from './lib/useSpring'
 export * from './lib/useSprings'
@@ -19,3 +12,40 @@ export * from './lib/useTransition'
 export * from './lib/useTrail'
 export * from './lib/useChain'
 export * from './lib/render-props'
+
+export {
+  config,
+  AnimatedValue,
+  AnimatedStyle,
+  SpringConfig,
+  TransitionPhase,
+} from './lib/common'
+
+/** Create a HOC that accepts `AnimatedValue` props */
+export const animated: <T extends ReactType>(
+  wrappedComponent: T
+) => AnimatedComponent<T> &
+  { [Tag in keyof JSX.IntrinsicElements]: AnimatedComponent<Tag> }
+
+/** The type of an `animated()` component */
+export type AnimatedComponent<T extends ReactType> = ForwardRefExoticComponent<
+  AnimatedProps<ComponentPropsWithRef<T>>
+>
+
+/** The props of an `animated()` component */
+export type AnimatedProps<Props extends object> = Solve<
+  { [P in keyof Props]: AnimatedProp<Props, P> }
+>
+
+type AnimatedProp<
+  Props extends object,
+  P extends keyof Props
+> = Props[P] extends infer T
+  ? P extends 'ref'
+    ? T
+    : P extends 'style'
+    ? AnimatedStyle<T>
+    : T extends CSSProperties
+    ? AnimatedStyle<T>
+    : T | AnimatedValue<Exclude<T, void>>
+  : never
