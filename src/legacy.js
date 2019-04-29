5ed81ad5bcbf0d525ead7d8ce2fef379a26cd6a3
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-29 16:33:22 -0400

    fix(Transition): when "children" is not a function
    Reported by: https://github.com/react-spring/react-spring/issues/642\#issuecomment-487653288

--- src/legacy.js
+++ src/legacy.js
@@ -15,8 +15,10 @@ export function Trail({ items, children, ...props }) {
 
 export function Transition({ items, keys = null, children, ...props }) {
   const transitions = useTransition(items, keys, props)
-  return transitions.map(({ item, key, props, slot }, index) => {
-    const el = children(item, slot, index)(props)
-    return <el.type key={key} {...el.props} />
-  })
+  return is.fun(children)
+    ? transitions.map(({ item, key, props, slot }, index) => {
+        const el = children(item, slot, index)(props)
+        return <el.type key={key} {...el.props} />
+      })
+    : null
 }
