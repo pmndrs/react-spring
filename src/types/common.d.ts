458d16cd1461e632b2cd383b2ae5bd1784fce854
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-30 09:45:44 -0400

    fix(ts): let `Controller#update` take a falsy argument

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -6,3 +6,7 @@ export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
 /** Ensure each type of `T` is an array */
 export type Arrify<T> = T extends ReadonlyArray<any> ? T : Readonly<[T]>
+
+export type Falsy = false | null | undefined
+
+export type OnEnd = (finished?: boolean) => void
