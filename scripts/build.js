be730a3cc7a1b1a7b66e0388841f53b7902bcdaa
Alec Larson <alec.stanford.larson@gmail.com>
2019-05-05 17:28:51 -0400

    build: better build script
    Add --only and --not flags for filtering which targets are bundled.
    
    Add --no-cookbook and --no-renderprops to disable specific bundles.
    
    Added dependencies were already installed by other devDependencies

---
+++ scripts/build.js
@@ -0,0 +1,74 @@
+#! /usr/bin/env node
+const { argv } = require('yargs') // 12.x
+const chalk = require('chalk') // 2.x
+const execa = require('execa') // 1.x
+const path = require('path')
+const fs = require('fs-extra') // 6.x
+
+const NPM_BIN = './node_modules/.bin'
+const BUILD_DIR = 'dist'
+
+const run = (...scripts) =>
+  Promise.all(
+    scripts.map(async script => {
+      if (!script) return
+      if (typeof script === 'string') {
+        let [cmd, ...args] = script.trim().split(' ')
+        console.log(chalk.gray('$ ' + cmd + ' ' + args.join(' ')))
+        if (cmd !== 'npm') {
+          cmd = path.join(NPM_BIN, cmd)
+        }
+        await execa(cmd, args, { stdio: 'inherit' })
+      }
+    })
+  )
+
+const toArray = val => (val == null ? [] : Array.isArray(val) ? val : [val])
+const getRollupArgv = () => {
+  const onlyArgs = toArray(argv.only).concat(argv.t || [])
+  const notArgs = toArray(argv.not).concat(argv.n || [])
+  return [
+    ...(!!argv.watch ? ['--watch'] : []),
+    ...onlyArgs.map(target => '--config-only ' + target),
+    ...notArgs.map(target => '--config-not ' + target),
+    '--config-cookbook ' + (!onlyArgs.length && argv.cookbook !== false),
+    '--config-renderprops ' + (!onlyArgs.length && argv.renderprops !== false),
+  ]
+}
+
+async function build() {
+  // Files copied into the $BUILD_DIR
+  const files = ['package.json', 'readme.md', 'LICENSE']
+
+  // Copy some metadata
+  await run(`cpx {${files.join(',')}} ${BUILD_DIR}`)
+
+  // Prepare the package.json for publishing
+  const oldJson = await preparePackageJson()
+
+  // Copy the types
+  await run(oldJson.scripts['copy:ts'])
+
+  // Bundle the modules
+  await run(`rollup -c ${getRollupArgv().join(' ')}`)
+}
+
+async function preparePackageJson() {
+  const packagePath = path.join(BUILD_DIR, 'package.json')
+  const oldJson = await fs.readJson(packagePath)
+
+  await fs.writeJson(packagePath, {
+    ...oldJson,
+    private: false,
+    devDependencies: undefined,
+    optionalDependencies: undefined,
+    scripts: undefined,
+    husky: undefined,
+    prettier: undefined,
+    jest: undefined,
+  })
+
+  return oldJson
+}
+
+build().catch(console.error)
