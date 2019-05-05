2efa5a05bfeeec1da7d0233be0ec72c77b828dc8
Alec Larson <alec.stanford.larson@gmail.com>
2019-05-05 17:28:51 -0400

    build: better build script
    Add --only and --not flags for filtering which targets are bundled.
    
    Add --no-cookbook and --no-renderprops to disable specific bundles.
    
    Added dependencies were already installed by other devDependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,52 @@
-import path from 'path'
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import { uglify } from 'rollup-plugin-uglify'
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+const path = require('path')
+const yargs = require('yargs')
+const babel = require('rollup-plugin-babel')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const { uglify } = require('rollup-plugin-uglify')
+const { sizeSnapshot } = require('rollup-plugin-size-snapshot')
+
+const DEFAULT_TARGET = 'web'
+
+const toArray = val => (val == null ? val : Array.isArray(val) ? val : [val])
+const isFalse = arg => /^(0|false)$/.test(arg)
+
+// Use the --only and --not flags to control which targets are built.
+let targets = ['web', 'native', 'universal', 'konva', 'three']
+
+/**
+ * Arguments
+ */
+
+const argv = { ...yargs.argv }
+for (const key in argv) {
+  // Rename any flags ignored by Rollup
+  if (key.startsWith('config-')) {
+    argv[key.slice(7)] = argv[key]
+  }
+}
+
+argv.only = toArray(argv.only)
+if (argv.only) {
+  targets = targets.filter(target => argv.only.includes(target))
+}
+
+argv.not = toArray(argv.not)
+if (argv.not) {
+  targets = targets.filter(target => !argv.not.includes(target))
+}
+
+argv.cookbook = !argv.only && !isFalse(argv.cookbook)
+argv.renderprops = !argv.only && !isFalse(argv.renderprops)
+
+/**
+ * Rollup configuration
+ */
 
 const root = process.platform === 'win32' ? path.resolve('/') : '/'
 const external = id => !id.startsWith('.') && !id.startsWith(root)
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
 const getBabelOptions = ({ useESModules }, targets) => ({
   babelrc: false,
   extensions,
@@ -56,7 +95,7 @@ function createConfig(entry, out) {
   ]
 }
 
-function createCjs(entry, out) {
+function createLegacyConfig(entry, out) {
   return [
     {
       input: `./src/${entry}/index`,
@@ -86,29 +125,54 @@ function createCjs(entry, out) {
   ]
 }
 
+// Apply an array of objects to an `Object.assign` call
+const flatten = (arr, out = []) => {
+  arr.forEach(val => {
+    if (val == null) return
+    if (Array.isArray(val)) flatten(val, out)
+    else out.push(val)
+  })
+  return out
+}
+
+// Map an array and pass it to `merge`
+const flatMap = (arr, mapper) => flatten(arr.map(mapper))
+
+// Filter an array to remove the given values
+const without = (arr, ...args) => arr.filter(val => !args.includes(val))
+
 export default [
-  ...createConfig('targets/web', 'web'),
-  ...createConfig('targets/native', 'native'),
-  ...createConfig('targets/universal', 'universal'),
-  ...createConfig('targets/konva', 'konva'),
-  ...createConfig('targets/three', 'three'),
-  {
-    input: `./src/targets/cookbook/index`,
-    output: { file: `dist/cookbook.js`, format: 'esm' },
-    external,
-    plugins: [
-      babel(
-        getBabelOptions(
-          { useESModules: true },
-          '>1%, not dead, not ie 11, not op_mini all'
+  ...flatMap(targets, target => createConfig('targets/' + target, target)),
+  /**
+   * react-spring/renderprops
+   */
+  ...(argv.renderprops !== false
+    ? flatMap(without(targets, 'three').concat('addons'), target =>
+        createLegacyConfig(
+          'renderprops/' + (target === 'addons' ? '' : 'targets/') + target,
+          'renderprops' + (target === DEFAULT_TARGET ? '' : '-' + target)
         )
-      ),
-      resolve({ extensions }),
-    ],
-  },
-  ...createCjs('renderprops/targets/web', 'renderprops'),
-  ...createCjs('renderprops/addons', 'renderprops-addons'),
-  ...createCjs('renderprops/targets/native', 'renderprops-native'),
-  ...createCjs('renderprops/targets/universal', 'renderprops-universal'),
-  ...createCjs('renderprops/targets/konva', 'renderprops-konva'),
+      )
+    : []),
+  /**
+   * react-spring/cookbook
+   */
+  ...(argv.cookbook !== false
+    ? [
+        {
+          input: `./src/targets/cookbook/index`,
+          output: { file: `dist/cookbook.js`, format: 'esm' },
+          external,
+          plugins: [
+            babel(
+              getBabelOptions(
+                { useESModules: true },
+                '>1%, not dead, not ie 11, not op_mini all'
+              )
+            ),
+            resolve({ extensions }),
+          ],
+        },
+      ]
+    : []),
 ]
