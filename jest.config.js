c3c6399eeecd2fa724cc007d8f249685e4327fee
Alec Larson <alec.stanford.larson@gmail.com>
2019-06-08 16:03:33 -0400

    chore: make jest work

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['**/src/**/*', '!**/dist/**/*', '!**/*.json'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['json', 'html', 'text'],
+  moduleNameMapper: {
+    '^shared$': '<rootDir>/packages/shared/src/index',
+    '^shared/(.*)': '<rootDir>/packages/shared/src/$1',
+  },
+  modulePathIgnorePatterns: ['.+/(dist|docs|examples)'],
+  testPathIgnorePatterns: ['.+/types/.+'],
+  testMatch: ['**/*.test.*'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+}
