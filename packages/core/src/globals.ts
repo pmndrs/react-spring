b2cc9979a9f3f910dc8dd3964d5db56a9be13fd1
Alec Larson <alec.stanford.larson@gmail.com>
2019-06-09 11:30:56 -0400

    refactor: use "@react-spring/shared/stringInterpolation" by default
    We could probably move this into core

--- packages/core/src/globals.ts
+++ packages/core/src/globals.ts
@@ -1,30 +1,6 @@
-import { Globals, InterpolatorConfig, createInterpolator } from 'shared'
+import { createStringInterpolator } from 'shared/stringInterpolation'
 import { FrameLoop } from './FrameLoop'
-
-// Problem: https://github.com/animatedjs/animated/pull/102
-// Solution: https://stackoverflow.com/questions/638565/parsing-scientific-notation-sensibly/658662
-const stringShapeRegex = /[+\-]?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?/g
-const createStringInterpolator = (config: InterpolatorConfig<string>) => {
-  const outputRange = config.output
-  const outputRanges: number[][] = outputRange[0]
-    .match(stringShapeRegex)!
-    .map(() => [])
-  outputRange.forEach(value => {
-    value
-      .match(stringShapeRegex)!
-      .forEach((number, i) => outputRanges[i].push(+number))
-  })
-  const interpolations = outputRange[0]
-    .match(stringShapeRegex)!
-    .map((_, i) => createInterpolator({ ...config, output: outputRanges[i] }))
-  return (input: number) => {
-    let i = 0
-    return outputRange[0].replace(
-      stringShapeRegex,
-      () => (interpolations[i++](input) as unknown) as string
-    )
-  }
-}
+import { Globals } from 'shared'
 
 Globals.assign({
   frameLoop: new FrameLoop(),
