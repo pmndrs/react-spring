183d54fd7a2eb0c3a9e0e02e747926a12a3e6e86
Alec Larson <alec.stanford.larson@gmail.com>
2019-06-12 09:53:45 -0400

    build: fix native postinstall
    The last iteration did not account for flattened node_modules.
    
    This checks our own node_modules and then the parent node_modules.

--- targets/native/scripts/_postinstall.js
+++ targets/native/scripts/_postinstall.js
@@ -1,10 +1,23 @@
 const fs = require('fs')
 const path = require('path')
-const mkdir = name => fs.existsSync(name) || fs.mkdirSync(name)
+const mkdir = dir => fs.existsSync(dir) || fs.mkdirSync(dir)
+const checkPath = file => fs.existsSync(file) && file
 
-process.chdir(__dirname)
-mkdir('node_modules')
+// Link "node_modules/shared" to wherever "@react-spring/shared" can be found.
+createAlias('shared', '@react-spring/shared')
 
-// Link "node_modules/shared" to "node_modules/@react-spring/shared"
-const target = '@react-spring/shared'
-fs.symlinkSync(target, 'node_modules/shared')
+function createAlias(alias, target) {
+  target =
+    // Look for "node_modules/@react-spring/{self}/node_modules/{target}"
+    checkPath(path.join(__dirname, 'node_modules', target)) ||
+    // Look for "node_modules/{target}"
+    checkPath(path.resolve(__dirname, '../..', target))
+
+  if (target) {
+    let origin = path.join(__dirname, 'node_modules')
+    mkdir(origin)
+    target = path.relative(origin, target)
+    origin = path.join(origin, alias)
+    fs.symlinkSync(target, origin)
+  }
+}
