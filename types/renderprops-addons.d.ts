71a833bec370890155912976ff57ece788da8c14
Ilya Sevostyanov <ruvata34@gmail.com>
2019-07-23 00:35:36 +0300

    FIX: add translated props corresponding typing
    Adding correct type definition to props signature of real implementation of Parallax and ParallaxLayer exported classes. Support for props translated to native elements 'as is'

--- types/renderprops-addons.d.ts
+++ types/renderprops-addons.d.ts
@@ -1,7 +1,7 @@
 import { Ref, PureComponent } from 'react'
 import { SpringConfig } from './renderprops-universal'
 
-interface ParallaxProps {
+interface ParallaxProps extends React.HTMLAttributes<HTMLElement> {
   pages: number
 
   config?: SpringConfig | ((key: string) => SpringConfig)
@@ -17,7 +17,7 @@ export class Parallax extends PureComponent<ParallaxProps> {
   scrollTo: (offset: number) => void
 }
 
-interface ParallaxLayerProps {
+interface ParallaxLayerProps extends React.HTMLAttributes<HTMLDivElement> {
   factor?: number
 
   offset?: number
