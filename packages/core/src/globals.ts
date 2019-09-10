0bc503c6f977814f37f01d5333c0b8b2681eed56
Alec Larson <alec.stanford.larson@gmail.com>
2019-09-10 19:12:16 -0400

    chore: fix circular import

--- packages/core/src/globals.ts
+++ packages/core/src/globals.ts
@@ -1,8 +1,10 @@
 import { createStringInterpolator } from 'shared/stringInterpolation'
 import { FrameLoop } from './FrameLoop'
 import { Globals } from 'shared'
+import { To } from './To'
 
 Globals.assign({
+  to: (source, args) => new To(source, args),
   frameLoop: new FrameLoop(),
   createStringInterpolator,
   applyAnimatedValues: () => false,
