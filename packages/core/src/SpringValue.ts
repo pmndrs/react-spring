95e658861d7c8bd8af0dddcbfd1a905e21fdec2c
Alec Larson <alec.stanford.larson@gmail.com>
2019-09-13 14:27:05 -0400

    fix: call "SpringValue#_start" when parent is a non-animated FluidValue
    Read the code comment below

--- packages/core/src/SpringValue.ts
+++ packages/core/src/SpringValue.ts
@@ -377,6 +377,11 @@ export class SpringValue<T = any, P extends string = string>
         this.set(value)
       }
     }
+    // When our parent is not a spring, it won't tell us to enter the frameloop
+    // because it never does so itself. Instead, we must react to value changes.
+    else if (!this.is(ACTIVE) && !isEqual(value, this.get())) {
+      this._start()
+    }
   }
 
   /** @internal */
