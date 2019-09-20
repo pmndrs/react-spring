b859e3a481305dfc067e8dfdfa4f87663ef0ebce
Alec Larson <alec.stanford.larson@gmail.com>
2019-09-20 16:05:55 -0400

    test: use shorthand "start" call

--- packages/core/src/Controller.test.ts
+++ packages/core/src/Controller.test.ts
@@ -50,28 +50,24 @@ it('can animate an array of numbers', () => {
 describe('async "to" prop', () => {
   it('acts strangely without the "from" prop', async () => {
     const ctrl = new Controller<{ x: number }>()
-    ctrl
-      .update({
-        to: async update => {
-          // The spring does not exist yet!
-          expect(ctrl.get('x')).toBeUndefined()
+    ctrl.start({
+      to: async update => {
+        // The spring does not exist yet!
+        expect(ctrl.get('x')).toBeUndefined()
 
-          // Any values passed here are treated as "from" values,
-          // because no "from" prop was ever given.
-          let promise = update({ x: 1 })
-          // Now the spring exists!
-          expect(ctrl.get('x')).toBeDefined()
-          // But the spring is idle!
-          expect(ctrl.get('x').idle).toBeTruthy()
-          await promise
+        // Any values passed here are treated as "from" values,
+        // because no "from" prop was ever given.
+        update({ x: 1 })
+        // Now the spring exists!
+        expect(ctrl.get('x')).toBeDefined()
+        // But the spring is idle!
+        expect(ctrl.get('x').idle).toBeTruthy()
 
-          // This call *will* start an animation!
-          promise = update({ x: 2 })
-          expect(ctrl.get('x').idle).toBeFalsy()
-          await promise
-        },
-      })
-      .start()
+        // This call *will* start an animation!
+        update({ x: 2 })
+        expect(ctrl.get('x').idle).toBeFalsy()
+      },
+    })
 
     // Since we call `update` twice, frames are generated!
     expect(await getAsyncFrames(ctrl)).toMatchSnapshot()
