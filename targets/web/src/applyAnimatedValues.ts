4dea3e1029ff6db9458c8f2453e690468ab0d943
Alec Larson <alec.stanford.larson@gmail.com>
2019-09-21 17:54:12 -0400

    nit: reorganize applyAnimatedValues in react-spring/web

--- targets/web/src/applyAnimatedValues.ts
+++ targets/web/src/applyAnimatedValues.ts
@@ -1,59 +1,5 @@
 import { Globals as G } from 'shared'
 
-let isUnitlessNumber: { [key: string]: true } = {
-  animationIterationCount: true,
-  borderImageOutset: true,
-  borderImageSlice: true,
-  borderImageWidth: true,
-  boxFlex: true,
-  boxFlexGroup: true,
-  boxOrdinalGroup: true,
-  columnCount: true,
-  columns: true,
-  flex: true,
-  flexGrow: true,
-  flexPositive: true,
-  flexShrink: true,
-  flexNegative: true,
-  flexOrder: true,
-  gridRow: true,
-  gridRowEnd: true,
-  gridRowSpan: true,
-  gridRowStart: true,
-  gridColumn: true,
-  gridColumnEnd: true,
-  gridColumnSpan: true,
-  gridColumnStart: true,
-  fontWeight: true,
-  lineClamp: true,
-  lineHeight: true,
-  opacity: true,
-  order: true,
-  orphans: true,
-  tabSize: true,
-  widows: true,
-  zIndex: true,
-  zoom: true,
-  // SVG-related properties
-  fillOpacity: true,
-  floodOpacity: true,
-  stopOpacity: true,
-  strokeDasharray: true,
-  strokeDashoffset: true,
-  strokeMiterlimit: true,
-  strokeOpacity: true,
-  strokeWidth: true,
-}
-
-const prefixKey = (prefix: string, key: string) =>
-  prefix + key.charAt(0).toUpperCase() + key.substring(1)
-const prefixes = ['Webkit', 'Ms', 'Moz', 'O']
-
-isUnitlessNumber = Object.keys(isUnitlessNumber).reduce((acc, prop) => {
-  prefixes.forEach(prefix => (acc[prefixKey(prefix, prop)] = acc[prop]))
-  return acc
-}, isUnitlessNumber)
-
 const isCustomPropRE = /^--/
 
 type Value = string | number | boolean | null
@@ -132,3 +78,57 @@ export function applyAnimatedValues(
     }
   })
 }
+
+let isUnitlessNumber: { [key: string]: true } = {
+  animationIterationCount: true,
+  borderImageOutset: true,
+  borderImageSlice: true,
+  borderImageWidth: true,
+  boxFlex: true,
+  boxFlexGroup: true,
+  boxOrdinalGroup: true,
+  columnCount: true,
+  columns: true,
+  flex: true,
+  flexGrow: true,
+  flexPositive: true,
+  flexShrink: true,
+  flexNegative: true,
+  flexOrder: true,
+  gridRow: true,
+  gridRowEnd: true,
+  gridRowSpan: true,
+  gridRowStart: true,
+  gridColumn: true,
+  gridColumnEnd: true,
+  gridColumnSpan: true,
+  gridColumnStart: true,
+  fontWeight: true,
+  lineClamp: true,
+  lineHeight: true,
+  opacity: true,
+  order: true,
+  orphans: true,
+  tabSize: true,
+  widows: true,
+  zIndex: true,
+  zoom: true,
+  // SVG-related properties
+  fillOpacity: true,
+  floodOpacity: true,
+  stopOpacity: true,
+  strokeDasharray: true,
+  strokeDashoffset: true,
+  strokeMiterlimit: true,
+  strokeOpacity: true,
+  strokeWidth: true,
+}
+
+const prefixKey = (prefix: string, key: string) =>
+  prefix + key.charAt(0).toUpperCase() + key.substring(1)
+const prefixes = ['Webkit', 'Ms', 'Moz', 'O']
+
+isUnitlessNumber = Object.keys(isUnitlessNumber).reduce((acc, prop) => {
+  prefixes.forEach(prefix => (acc[prefixKey(prefix, prop)] = acc[prop]))
+  return acc
+}, isUnitlessNumber)
