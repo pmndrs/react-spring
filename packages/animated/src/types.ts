22d42f0c69d08c7d9c941c852631359b7449e22a
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-06 15:18:20 -0400

    nit: remove unnecessary type intersection

--- packages/animated/src/types.ts
+++ packages/animated/src/types.ts
@@ -1,4 +1,3 @@
-import { Animated } from './Animated'
 import { AnimatedArray } from './AnimatedArray'
 import { AnimatedValue } from './AnimatedValue'
 
@@ -6,8 +5,7 @@ export type AnimatedType<T = any> = Function & {
   create: (
     from: any,
     goal?: any
-  ) => Animated &
-    (T extends ReadonlyArray<number | string>
-      ? AnimatedArray<T>
-      : AnimatedValue<T>)
+  ) => T extends ReadonlyArray<number | string>
+    ? AnimatedArray<T>
+    : AnimatedValue<T>
 }
