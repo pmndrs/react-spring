f609e2c3e63654837f4c2daefc1d0f18abc74b93
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-13 11:55:33 -0500

    test: "set" method of SpringValue

--- packages/core/src/SpringValue.test.ts
+++ packages/core/src/SpringValue.test.ts
@@ -34,4 +34,39 @@ describe('SpringValue', () => {
       toValues: [1],
     })
   })
+
+  describe('"set" method', () => {
+    it('stops the active animation', async () => {
+      const spring = new SpringValue(0)
+      const promise = spring.start(1)
+
+      advanceUntilValue(spring, 0.5)
+      spring.set(2)
+
+      expect(spring.idle).toBeTruthy()
+      expect(await promise).toMatchObject({
+        finished: false,
+        value: 2,
+      })
+    })
+
+    describe('when a new value is passed', () => {
+      it('calls the "onChange" prop', () => {
+        const onChange = jest.fn()
+        const spring = new SpringValue(0, { onChange })
+        spring.set(1)
+        expect(onChange).toBeCalledWith(1, spring)
+      })
+      it.todo('wraps the "onChange" call with "batchedUpdates"')
+    })
+
+    describe('when the current value is passed', () => {
+      it('skips the "onChange" call', () => {
+        const onChange = jest.fn()
+        const spring = new SpringValue(0, { onChange })
+        spring.set(0)
+        expect(onChange).not.toBeCalled()
+      })
+    })
+  })
 })
