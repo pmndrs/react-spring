f235b0672ffde3a82fbcadb5772f09399b89b03c
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-25 18:14:58 -0500

    test: when target is a SpringValue and its "set" method is used
    And reorganize some tests

--- packages/core/src/SpringValue.test.ts
+++ packages/core/src/SpringValue.test.ts
@@ -12,29 +12,6 @@ describe('SpringValue', () => {
     expect(frames).toMatchSnapshot()
   })
 
-  it('can animate toward another SpringValue', () => {
-    const spring1 = new SpringValue(0)
-    const spring2 = new SpringValue(1)
-
-    spring1.start({ to: spring2 })
-    expect(spring1.priority).toBeGreaterThan(spring2.priority)
-    expect(spring1.animation).toMatchObject({
-      to: spring2,
-      toValues: null,
-    })
-
-    advanceUntilIdle()
-    expect(spring1.get()).toBe(1)
-
-    // It can switch its target to the current value.
-    spring1.start(1)
-    expect(spring1.priority).toBe(spring2.priority)
-    expect(spring1.animation).toMatchObject({
-      to: 1,
-      toValues: [1],
-    })
-  })
-
   describe('"set" method', () => {
     it('stops the active animation', async () => {
       const spring = new SpringValue(0)
@@ -66,6 +43,86 @@ describe('SpringValue', () => {
         const spring = new SpringValue(0, { onChange })
         spring.set(0)
         expect(onChange).not.toBeCalled()
+      })
+    })
+  })
+
+  describe('when our target is another SpringValue', () => {
+    it('animates toward the current value', () => {
+      const spring = new SpringValue(0)
+      const target = new SpringValue(1)
+
+      spring.start({ to: target })
+      expect(spring.priority).toBeGreaterThan(target.priority)
+      expect(spring.animation).toMatchObject({
+        to: target,
+        toValues: null,
+      })
+
+      advanceUntilIdle()
+      expect(spring.get()).toBe(1)
+    })
+
+    it('can change its target while animating', () => {
+      const spring = new SpringValue(0)
+      const target = new SpringValue(1)
+
+      spring.start({ to: target })
+      advanceUntilValue(spring, 0.5)
+
+      spring.start(0)
+      expect(spring.priority).toBe(target.priority)
+      expect(spring.animation).toMatchObject({
+        to: 0,
+        toValues: [0],
+      })
+
+      advanceUntilIdle()
+      expect(spring.get()).toBe(0)
+    })
+
+    describe('when target is done animating', () => {
+      it('keeps animating until the target is reached', () => {
+        const spring = new SpringValue(0)
+        const target = new SpringValue(1)
+
+        spring.start({ to: target })
+        target.start({ to: 1.1 })
+
+        advanceUntil(() => target.idle)
+        expect(spring.idle).toBeFalsy()
+
+        advanceUntilIdle()
+        expect(spring.idle).toBeTruthy()
+        expect(spring.get()).toBe(1.1)
+      })
+    })
+
+    describe('when target animates after we go idle', () => {
+      it('starts animating', () => {
+        const spring = new SpringValue(0)
+        const target = new SpringValue(1)
+
+        spring.start({ to: target })
+        advanceUntil(() => spring.idle)
+
+        target.start({ to: 2 })
+        expect(getFrames(spring).length).toBeGreaterThan(1)
+        expect(spring.get()).toBe(2)
+      })
+    })
+
+    describe('when target has its value set (not animated)', () => {
+      it('animates toward the new value', () => {
+        const spring = new SpringValue(0)
+        const target = new SpringValue(1)
+
+        spring.start({ to: target })
+        advanceUntilIdle()
+
+        target.set(2)
+        expect(getFrames(spring).length).toBeGreaterThan(1)
+        expect(spring.get()).toBe(2)
       })
     })
   })
