132eea87660e9009d7af3bf7db36658e9680c6b9
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-26 19:12:25 -0500

    test: add stubs for Interpolation class

---
+++ packages/core/src/Interpolation.test.ts
@@ -0,0 +1,13 @@
+describe('Interpolation', () => {
+  it.todo('can use a SpringValue')
+  it.todo('can use another Interpolation')
+  it.todo('can use a non-animated FluidValue')
+
+  describe('when multiple inputs change in the same frame', () => {
+    it.todo('only computes its value once')
+  })
+
+  describe('when an input resets its animation', () => {
+    it.todo('computes its value before the first frame')
+  })
+})
