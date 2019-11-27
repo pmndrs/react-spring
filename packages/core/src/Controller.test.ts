11aedd4f98ca78d7b0ec2c93d5f290503e889093
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-26 23:23:51 -0500

    chore: wrap Controller tests in a describe block

--- packages/core/src/Controller.test.ts
+++ packages/core/src/Controller.test.ts
@@ -1,60 +1,62 @@
 import { Controller } from './Controller'
 
-it('can animate a number', () => {
-  const ctrl = new Controller({ x: 0 })
-  ctrl.start({ x: 100 })
+describe('Controller', () => {
+  it('can animate a number', () => {
+    const ctrl = new Controller({ x: 0 })
+    ctrl.start({ x: 100 })
 
-  const frames = getFrames(ctrl)
-  expect(frames).toMatchSnapshot()
+    const frames = getFrames(ctrl)
+    expect(frames).toMatchSnapshot()
 
-  // The first frame should *not* be the from value.
-  expect(frames[0]).not.toEqual({ x: 0 })
+    // The first frame should *not* be the from value.
+    expect(frames[0]).not.toEqual({ x: 0 })
 
-  // The last frame should be the goal value.
-  expect(frames.slice(-1)[0]).toEqual({ x: 100 })
-})
+    // The last frame should be the goal value.
+    expect(frames.slice(-1)[0]).toEqual({ x: 100 })
+  })
 
-it('can animate an array of numbers', () => {
-  const config = { precision: 0.005 }
-  const ctrl = new Controller<{ x: [number, number] }>({ x: [1, 2], config })
-  ctrl.start({ x: [5, 10] })
+  it('can animate an array of numbers', () => {
+    const config = { precision: 0.005 }
+    const ctrl = new Controller<{ x: [number, number] }>({ x: [1, 2], config })
+    ctrl.start({ x: [5, 10] })
 
-  const frames = getFrames(ctrl)
-  expect(frames).toMatchSnapshot()
+    const frames = getFrames(ctrl)
+    expect(frames).toMatchSnapshot()
 
-  // The last frame should be the goal value.
-  expect(frames.slice(-1)[0]).toEqual({ x: [5, 10] })
+    // The last frame should be the goal value.
+    expect(frames.slice(-1)[0]).toEqual({ x: [5, 10] })
 
-  // The 2nd value is always ~2x the 1st value (within the defined precision).
-  const factors = frames.map(frame => frame.x[1] / frame.x[0])
-  expect(
-    factors.every(factor => Math.abs(2 - factor) < config.precision)
-  ).toBeTruthy()
-})
+    // The 2nd value is always ~2x the 1st value (within the defined precision).
+    const factors = frames.map(frame => frame.x[1] / frame.x[0])
+    expect(
+      factors.every(factor => Math.abs(2 - factor) < config.precision)
+    ).toBeTruthy()
+  })
 
-describe('async "to" prop', () => {
-  it('acts strangely without the "from" prop', async () => {
-    const ctrl = new Controller<{ x: number }>()
-    ctrl.start({
-      to: async update => {
-        // The spring does not exist yet!
-        expect(ctrl.get('x')).toBeUndefined()
+  describe('when the "to" prop is an async function', () => {
+    it('acts strangely without the "from" prop', async () => {
+      const ctrl = new Controller<{ x: number }>()
+      ctrl.start({
+        to: async update => {
+          // The spring does not exist yet!
+          expect(ctrl.get('x')).toBeUndefined()
 
-        // Any values passed here are treated as "from" values,
-        // because no "from" prop was ever given.
-        update({ x: 1 })
-        // Now the spring exists!
-        expect(ctrl.get('x')).toBeDefined()
-        // But the spring is idle!
-        expect(ctrl.get('x').idle).toBeTruthy()
+          // Any values passed here are treated as "from" values,
+          // because no "from" prop was ever given.
+          update({ x: 1 })
+          // Now the spring exists!
+          expect(ctrl.get('x')).toBeDefined()
+          // But the spring is idle!
+          expect(ctrl.get('x').idle).toBeTruthy()
 
-        // This call *will* start an animation!
-        update({ x: 2 })
-        expect(ctrl.get('x').idle).toBeFalsy()
-      },
+          // This call *will* start an animation!
+          update({ x: 2 })
+          expect(ctrl.get('x').idle).toBeFalsy()
+        },
+      })
+
+      // Since we call `update` twice, frames are generated!
+      expect(await getAsyncFrames(ctrl)).toMatchSnapshot()
     })
-
-    // Since we call `update` twice, frames are generated!
-    expect(await getAsyncFrames(ctrl)).toMatchSnapshot()
   })
 })
