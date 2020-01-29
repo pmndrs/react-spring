ddc241b78fbdfb61acd0f9b69f24c6510a3dc53f
1natsu172 <1natsu172@users.noreply.github.com>
2020-01-30 03:56:46 +0900

    feat: support ref prop to useTrail

--- packages/core/src/useTrail.ts
+++ packages/core/src/useTrail.ts
@@ -1,12 +1,18 @@
+import { useImperativeHandle } from 'react'
 import { useLayoutEffect } from 'react-layout-effect'
 import { is, UnknownProps } from 'shared'
 
 import { PickAnimated, Valid } from './types/common'
-import { SpringValues, SpringStopFn, SpringsUpdateFn } from './types/spring'
+import {
+  SpringValues,
+  SpringStopFn,
+  SpringsUpdateFn,
+  SpringHandle,
+} from './types/spring'
 import { UseSpringProps } from './useSpring'
-import { Controller } from './Controller'
+import { Controller, ControllerProps } from './Controller'
 import { useSprings } from './useSprings'
-import { getProps } from './helpers'
+import { getProps, callProp } from './helpers'
 
 export function useTrail<Props extends object>(
   length: number,
@@ -27,8 +33,13 @@ export function useTrail<Props extends object>(
   deps?: any[]
 ): SpringValues<PickAnimated<Props>>[]
 
-export function useTrail(length: number, propsArg: unknown, deps?: any[]) {
+export function useTrail<Props extends object>(
+  length: number,
+  propsArg: (Props & Valid<Props, UseSpringProps<Props>>) | UseSpringProps,
+  deps?: any[]
+) {
   const propsFn = is.fun(propsArg) && propsArg
+  const calledProps = callProp<UseSpringProps>(propsArg)
 
   if (propsFn && arguments.length < 3) {
     deps = [] // Skip updates after first render.
@@ -62,6 +73,34 @@ export function useTrail(length: number, propsArg: unknown, deps?: any[]) {
       return props
     })
   }
+
+  useImperativeHandle(
+    calledProps.ref,
+    (): SpringHandle<ControllerProps> => ({
+      get controllers() {
+        return ctrls
+      },
+      update(props) {
+        ctrls.forEach((ctrl, i) =>
+          ctrl.update(
+            is.fun(props) ? props(i, ctrl) : is.arr(props) ? props[i] : props
+          )
+        )
+        return this
+      },
+      async start() {
+        const results = await Promise.all(ctrls.map(ctrl => ctrl.start()))
+        return {
+          value: results.map(result => result.value),
+          finished: results.every(result => result.finished),
+        }
+      },
+      stop: keys => ctrls.forEach(ctrl => ctrl.stop(keys)),
+      pause: keys => ctrls.forEach(ctrl => ctrl.pause(keys)),
+      resume: keys => ctrls.forEach(ctrl => ctrl.resume(keys)),
+    }),
+    [calledProps.ref]
+  )
 
   return propsFn ? result : result[0]
 }
