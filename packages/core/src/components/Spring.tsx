3814010b5e2741b7a578ed6975fd156653643d83
Alec Larson <alec.stanford.larson@gmail.com>
2020-02-05 12:13:00 -0500

    types: fix Spring component type
    Animated values are now properly inferred.
    
    The "to" prop is used when it's an object. Otherwise, the "from" prop is used.

--- packages/core/src/components/Spring.tsx
+++ packages/core/src/components/Spring.tsx
@@ -1,20 +1,30 @@
-import { ReactNode } from 'react'
-
-import { PickAnimated, Valid } from '../types/common'
-import { SpringValues } from '../types/spring'
+import { NoInfer, UnknownProps } from '../types/common'
+import { SpringValues, AsyncTo } from '../types/spring'
 import { UseSpringProps, useSpring } from '../hooks/useSpring'
 
-export type SpringComponentProps<Props extends object = any> = unknown &
-  UseSpringProps<Props> & {
-    children: (values: SpringValues<PickAnimated<Props>>) => ReactNode
+export type SpringComponentProps<
+  State extends object = UnknownProps
+> = unknown &
+  UseSpringProps<State> & {
+    children: (values: SpringValues<State>) => JSX.Element | null
   }
+
+// Infer state from "from" object prop.
+export function Spring<State extends object>(
+  props: {
+    from: State
+    to?: AsyncTo<NoInfer<State>>
+  } & Omit<SpringComponentProps<NoInfer<State>>, 'from' | 'to'>
+): JSX.Element | null
+
+// Infer state from "to" object prop.
+export function Spring<State extends object>(
+  props: { to: State } & Omit<SpringComponentProps<NoInfer<State>>, 'to'>
+): JSX.Element | null
 
 /**
  * The `Spring` component passes `SpringValue` objects to your render prop.
  */
-export function Spring<Props extends SpringComponentProps>({
-  children,
-  ...props
-}: Props & Valid<Props, SpringComponentProps<Props>>) {
+export function Spring({ children, ...props }: any) {
   return children(useSpring(props))
 }
