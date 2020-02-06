7bd5efdd7b22848e351253b6d286731831cc7498
Alec Larson <alec.stanford.larson@gmail.com>
2020-02-06 15:44:10 -0500

    types: use FluidProps from "fluids" package

--- packages/shared/src/types/animated.ts
+++ packages/shared/src/types/animated.ts
@@ -1,10 +1,3 @@
-import { FluidValue } from 'fluids'
-
-/** Add the `FluidValue` type to every property */
-export type FluidProps<T> = T extends object
-  ? { [P in keyof T]: T[P] | FluidValue<Exclude<T[P], void>> }
-  : unknown
-
 /** These types can be animated */
 export type Animatable<T = any> = T extends number
   ? number
