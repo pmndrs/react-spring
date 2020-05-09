e0a6ee036506a20d7b6719c054db88bb50d1b77e
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-05-09 14:53:44 -0400

    fix: let r3f drive the frameloop

--- targets/three/src/index.ts
+++ targets/three/src/index.ts
@@ -1,14 +1,22 @@
-import { applyProps } from 'react-three-fiber'
-import { Globals } from 'core'
+import { applyProps, addEffect, invalidate } from 'react-three-fiber'
+import { Globals, FrameLoop } from 'core'
 import { createHost } from 'animated'
 import { createStringInterpolator } from 'shared/stringInterpolation'
 import colorNames from 'shared/colors'
 import { primitives } from './primitives'
 import { WithAnimated } from './animated'
 
+// Let r3f drive the frameloop.
+const frameLoop = new FrameLoop(() => invalidate())
+addEffect(() => {
+  frameLoop.advance()
+  return true // Never stop.
+})
+
 Globals.assign({
   createStringInterpolator,
   colorNames,
+  frameLoop,
 })
 
 const host = createHost(primitives, {
