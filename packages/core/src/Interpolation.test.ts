7e350271effba4f1a7c874012ad0565b467829ab
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-08-31 16:54:38 -0400

    test: when all `Interpolation` inputs are paused

--- packages/core/src/Interpolation.test.ts
+++ packages/core/src/Interpolation.test.ts
@@ -1,3 +1,6 @@
+import { SpringValue } from './SpringValue'
+import { to } from './interpolate'
+
 describe('Interpolation', () => {
   it.todo('can use a SpringValue')
   it.todo('can use another Interpolation')
@@ -9,5 +12,44 @@ describe('Interpolation', () => {
 
   describe('when an input resets its animation', () => {
     it.todo('computes its value before the first frame')
+  })
+
+  describe('when all inputs are paused', () => {
+    it('leaves the frameloop', () => {
+      const a = new SpringValue({ from: 0, to: 1 })
+      const b = new SpringValue({ from: 1, to: 0 })
+      mockRaf.step()
+
+      const calc = jest.fn((a: number, b: number) => Math.abs(a - b))
+      const c = to([a, b], calc)
+
+      // For interpolation to be active, it must be observed.
+      const observer = jest.fn()
+      c.addChild({ onParentChange: observer })
+
+      // Pause the first input.
+      a.pause()
+
+      // Expect interpolation to continue.
+      calc.mockClear()
+      mockRaf.step()
+      expect(calc).toBeCalled()
+
+      // Pause the other input.
+      b.pause()
+
+      // In the next frame, the interpolation still calculates its next value.
+      // When its value stays the same, it checks the idle status of each input,
+      // which triggers an update to its own idle status.
+      calc.mockClear()
+      mockRaf.step()
+      expect(calc).toBeCalled()
+      expect(c.idle).toBeTruthy()
+
+      // Expect interpolation to be paused.
+      calc.mockClear()
+      mockRaf.step()
+      expect(calc).not.toBeCalled()
+    })
   })
 })
