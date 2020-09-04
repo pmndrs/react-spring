31779f536693a10f69c4cf89e30bc7fea2958847
José Muñoz <jdmg93@gmail.com>
2020-09-03 20:46:12 -0700

    Adds injection of index param on functional API
    This commit preserves current behaviour of useTrail and adds the injection of the index parameter on the functional API of the hook matching the funcitonal API of useSprings, sans Controller injection.

--- src/useTrail.js
+++ src/useTrail.js
@@ -4,18 +4,25 @@ import { useSprings } from './useSprings'
 
 /** API
  * const trails = useTrail(number, { ... })
- * const [trails, set] = useTrail(number, () => ({ ... }))
+ * const [trails, set] = useTrail(number, (index) => ({ ... }))
  */
 
 export const useTrail = (length, props) => {
   const mounted = useRef(false)
-  const isFn = is.fun(props)
-  const updateProps = callProp(props)
+  const isFn = is.fun(props)  
   const instances = useRef()
+  const currentProps = useRef()
+  const [reverse, setReverse] = useState(false)
 
   const [result, set, pause] = useSprings(length, (i, ctrl) => {
-    if (i === 0) instances.current = []
+    const updateProps = callProp(props, i)
+    if (i === 0) {
+      instances.current = []
+      currentProps.current = []
+      setReverse(updateProps.reverse)
+    }
     instances.current.push(ctrl)
+    currentProps.current.push(updateProps)
     return {
       ...updateProps,
       config: callProp(updateProps.config, i),
@@ -27,6 +34,7 @@ export const useTrail = (length, props) => {
   const updateCtrl = useMemo(
     () => props =>
       set((i, ctrl) => {
+        const updateProps = currentProps.current[i]
         const last = props.reverse ? i === 0 : length - 1 === i
         const attachIdx = props.reverse ? i + 1 : i - 1
         const attachController = instances.current[attachIdx]
@@ -36,7 +44,7 @@ export const useTrail = (length, props) => {
           attach: attachController && (() => attachController),
         }
       }),
-    [length, updateProps.reverse]
+    [length, reverse]
   )
   // Update controller if props aren't functional
   useEffect(() => void (mounted.current && !isFn && updateCtrl(props)))
