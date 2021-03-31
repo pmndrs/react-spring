f5f3b3680739a721bd2c5482cfb90128b31f555e
David Bismut <david.bismut@gmail.com>
2021-03-31 16:53:50 +0200

    chore: add alloc/types missing types

--- packages/types/util.d.ts
+++ packages/types/util.d.ts
@@ -20,6 +20,9 @@
  * SOFTWARE.
  */
 
+import * as React from 'react'
+import { ReactElement, MutableRefObject } from 'react'
+
 /** Ensure each type of `T` is an array */
 export type Arrify<T> = [T, T] extends [infer T, infer DT]
   ? DT extends ReadonlyArray<any>
@@ -128,3 +131,28 @@ export type Exclusive<T> = keyof T extends infer Keys
 export interface Disposable {
   dispose(): void
 }
+
+// react.d.ts
+export type RefProp<T> = MutableRefObject<T | null | undefined>
+
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34237
+export type ElementType<P = any> =
+  | React.ElementType<P>
+  | LeafFunctionComponent<P>
+
+// Function component without children
+type LeafFunctionComponent<P> = {
+  (props: P): ReactElement | null
+  displayName?: string
+}
+
+export type ComponentPropsWithRef<
+  T extends ElementType
+> = T extends React.ComponentClass<infer P>
+  ? React.PropsWithoutRef<P> & React.RefAttributes<InstanceType<T>>
+  : React.PropsWithRef<React.ComponentProps<T>>
+
+// In @types/react, a "children" prop is required by the "FunctionComponent" type.
+export type ComponentType<P = {}> =
+  | React.ComponentClass<P>
+  | LeafFunctionComponent<P>
