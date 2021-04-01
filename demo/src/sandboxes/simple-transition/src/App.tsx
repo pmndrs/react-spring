da484fe43bbf84aa1168a903d9150cf000649c6f
David Bismut <david.bismut@gmail.com>
2021-04-01 18:07:11 +0200

    fix: set keys to null to generate unique ids

--- demo/src/sandboxes/simple-transition/src/App.tsx
+++ demo/src/sandboxes/simple-transition/src/App.tsx
@@ -13,6 +13,7 @@ export default function App() {
   const [index, set] = useState(0)
   const onClick = useCallback(() => set(state => (state + 1) % 3), [])
   const transitions = useTransition(index, {
+    keys: null,
     from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
     enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
     leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
