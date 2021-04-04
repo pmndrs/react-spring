015f9a545eb8bb026b0f3734ae04c6715ad71059
David Bismut <david.bismut@gmail.com>
2021-04-04 19:59:27 +0200

    fix: normalize onRest signature with async

--- packages/core/src/Controller.ts
+++ packages/core/src/Controller.ts
@@ -241,7 +241,7 @@ export class Controller<State extends Lookup = Lookup> {
       this._started = false
       flush(onRest, ([onRest, result]) => {
         result.value = values
-        onRest(result, this)
+        onRest(result.value, this)
       })
     }
   }
