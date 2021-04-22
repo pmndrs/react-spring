4d07bb45dc970b7968c759f0e32ac27075fe703e
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-04-22 22:25:01 +0100

    chore(demos): add parallax demos

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -21,6 +21,8 @@ import SvgFilter from './sandboxes/svg-filter/src/App'
 import CssKeyframes from './sandboxes/css-keyframes/src/App'
 import NotificationHub from './sandboxes/notification-hub/src/App'
 import Tree from './sandboxes/tree/src/App'
+import Parallax from './sandboxes/parallax/src/App'
+import ParallaxVert from './sandboxes/parallax-vert/src/App'
 
 const links = {
   card: Card,
@@ -42,6 +44,8 @@ const links = {
   'css-keyframes': CssKeyframes,
   'notification-hub': NotificationHub,
   tree: Tree,
+  parallax: Parallax,
+  'parallax-vert': ParallaxVert,
 }
 
 const Example = ({ link }) => {
