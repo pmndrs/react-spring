ece308221c4bbcb911cf473ce185becdfd9f5884
Lewis <davanzo.lewis@gmail.com>
2021-05-10 13:21:50 -0400

    feat: make demo more presentable

--- demo/src/sandboxes/parallax-sticky/src/App.tsx
+++ demo/src/sandboxes/parallax-sticky/src/App.tsx
@@ -1,36 +1,33 @@
 import React from 'react'
-import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import styles from './styles.module.css'
 
 export default function App() {
-  const flexCenter = { display: 'flex', justifyContent: 'center', alignItems: 'center' }
+  const alignCenter = { display: 'flex', alignItems: 'center' }
   return (
     <>
-      <div
-        style={{
-          background: 'linear-gradient(#e66465, #9198e5)',
-          position: 'absolute',
-          top: '0',
-          width: '100vw',
-          height: '100vh',
-        }}
-      />
+      <div className={styles.background} />
 
       <Parallax pages={5}>
-        <ParallaxLayer offset={1} speed={1.5}>
-          <div style={{ width: '25%', height: '100%', marginRight: 'auto', backgroundColor: 'rebeccapurple' }} />
+        <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
+          <p className={styles.scrollText}>Scroll down</p>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1} speed={-2.5}>
-          <div style={{ width: '25%', height: '100%', marginLeft: 'auto', backgroundColor: 'green' }} />
+        <ParallaxLayer sticky={{ from: 1, to: 3 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
+          <div className={`${styles.card} ${styles.sticky}`}>
+            <p>I'm a sticky layer</p>
+          </div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.5} style={{ ...flexCenter }}>
-          <p>Scroll down</p>
+        <ParallaxLayer offset={1.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+          <div className={`${styles.card} ${styles.parallax} ${styles.purple}`}>
+            <p>I'm not</p>
+          </div>
         </ParallaxLayer>
 
-        <ParallaxLayer sticky={{ from: 1, to: 3 }} style={{ ...flexCenter }}>
-          <div style={{ ...flexCenter, height: '10rem', width: '50%', backgroundColor: '#ff6d6d' }}>
-            <p>Should be sticky</p>
+        <ParallaxLayer offset={2.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+          <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
+            <p>Neither am I</p>
           </div>
         </ParallaxLayer>
       </Parallax>
