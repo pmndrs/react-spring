6af5a49997e639fa845b29684a27ff9cce232def
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-06-09 21:07:42 +0100

    chore: bump react to 18

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+
+if (!rootElement) throw new Error('Failed to find the root element')
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<App />)
