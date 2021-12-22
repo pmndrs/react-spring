f14ceebe44410d2dca8e68eaa6d053a315e8c2ae
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-12-22 22:24:36 +0000

    docs: improve responsive behaviour

--- docs/src/pages/_app.tsx
+++ docs/src/pages/_app.tsx
@@ -1,19 +1,17 @@
 import { AppProps } from 'next/app'
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { DefaultSeo } from 'next-seo'
 import { MDXProvider } from '@mdx-js/react'
 
 import { GLOBAL } from 'styles/global'
 import { RESET } from 'styles/reset'
 import { MARKDOWN } from 'styles/markdown'
+import { SpringTheme } from 'styles/theme'
 
-import { Splash } from 'components/Splash'
-import { ScrollIndicator } from 'components/ScrollIndicator'
+import { Header } from 'components/Splash'
 import { PageContainer } from 'components/PageContainer'
-import Footer from 'components/Footer'
+import { Footer } from 'components/Footer'
 import { CodeBlock } from 'components/CodeBlock'
-
-import { useManageScroll } from 'hooks/useManageScroll'
 
 import { DEFAULT_SEO } from 'references/defaultSeo'
 
@@ -30,19 +28,18 @@ const components = {
 }
 
 function App({ Component, pageProps }: MyAppProps) {
-  useManageScroll()
-
   return (
-    <MDXProvider components={components}>
-      <DefaultSeo {...DEFAULT_SEO} />
-      <Splash />
-      <ScrollIndicator />
-      <PageContainer>
-        <Component {...pageProps} />
-      </PageContainer>
-      <Footer />
-      <GlobalStyle />
-    </MDXProvider>
+    <ThemeProvider theme={SpringTheme}>
+      <MDXProvider components={components}>
+        <DefaultSeo {...DEFAULT_SEO} />
+        <Header />
+        <PageContainer>
+          <Component {...pageProps} />
+        </PageContainer>
+        <Footer />
+        <GlobalStyle />
+      </MDXProvider>
+    </ThemeProvider>
   )
 }
 
