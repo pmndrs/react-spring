a692bb991b76799cbfe81482fd4263aba1ae1106
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-02-20 16:02:34 +0000

    fix: window scrolling bug (should use refs)

--- docs-new/app/hooks/useWindowScrolling.ts
+++ docs-new/app/hooks/useWindowScrolling.ts
@@ -1,5 +1,5 @@
 import { raf } from '@react-spring/rafz'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useIsomorphicLayoutEffect } from './useIsomorphicEffect'
 
 type SCROLL_UP = 'up'
@@ -22,13 +22,13 @@ export const useWindowScrolling: UseWindowScrolling = ({
   const [direction, setDirection] = useState<SCROLL_DIR | undefined>(undefined)
   const [scrollTop, setScrollTop] = useState(0)
 
-  useIsomorphicLayoutEffect(() => {
-    let lastScrollY = 0
-    let ticking = false
+  const lastScrollY = useRef(0)
+  const ticking = useRef(false)
 
+  useIsomorphicLayoutEffect(() => {
     const updateScrollDir = () => {
       const scrollY = window.pageYOffset
-      const direction = scrollY > lastScrollY ? 'down' : 'up'
+      const direction = scrollY > lastScrollY.current ? 'down' : 'up'
 
       const thresholdValue = Array.isArray(threshold)
         ? threshold[direction === 'down' ? 0 : 1]
@@ -40,26 +40,26 @@ export const useWindowScrolling: UseWindowScrolling = ({
       }
 
       if (scrollY < yOffset) {
-        ticking = false
+        ticking.current = false
         return
       }
 
-      if (Math.abs(scrollY - lastScrollY) < thresholdValue) {
-        ticking = false
+      if (Math.abs(scrollY - lastScrollY.current) < thresholdValue) {
+        ticking.current = false
         return
       }
+
+      lastScrollY.current = scrollY > 0 ? scrollY : 0
+      ticking.current = false
 
       setDirection(direction)
       setScrollTop(scrollY)
-
-      lastScrollY = scrollY > 0 ? scrollY : 0
-      ticking = false
     }
 
     const handleScroll = (e?: Event) => {
-      if (!ticking) {
+      if (!ticking.current) {
         raf(updateScrollDir)
-        ticking = true
+        ticking.current = true
       } else if (onScroll && e) {
         onScroll(e)
       }
