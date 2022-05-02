9295fd1323caa92b5a8c61304defa906e01a306f
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-05-02 22:37:23 +0100

    chore: bump @types packages and fix TS errors

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
