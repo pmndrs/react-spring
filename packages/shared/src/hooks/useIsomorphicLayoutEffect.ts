8b97d8855ca79299f80eb9364070f5ee58a44abc
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-05-05 20:50:26 +0100

    chore: update useIsomorphicLayoutEffect comment

--- packages/shared/src/hooks/useIsomorphicLayoutEffect.ts
+++ packages/shared/src/hooks/useIsomorphicLayoutEffect.ts
@@ -1,10 +1,12 @@
 import { useEffect, useLayoutEffect } from 'react'
 
-// For server-side rendering: https://github.com/react-spring/zustand/pull/34
-// Deno support: https://github.com/pmndrs/zustand/issues/347
-const isSSR =
-  typeof window === 'undefined' ||
-  !window.navigator ||
-  /ServerSideRendering|^Deno\//.test(window.navigator.userAgent)
+import { isSSR } from '../helpers'
 
-export const useIsomorphicLayoutEffect = isSSR ? useEffect : useLayoutEffect
+/**
+ * Use this to read layout from the DOM and synchronously
+ * re-render if the isSSR returns true. Updates scheduled
+ * inside `useIsomorphicLayoutEffect` will be flushed
+ * synchronously in the browser, before the browser has
+ * a chance to paint.
+ */
+export const useIsomorphicLayoutEffect = isSSR() ? useEffect : useLayoutEffect
