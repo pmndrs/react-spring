8b5aaefd53fcb8e22f55d037ab0ffcb5a5a38891
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-06-29 20:36:33 +0100

    chore: re-work build sequence for docs

--- docs/remix.config.js
+++ docs/remix.config.js
@@ -9,13 +9,9 @@ const rehypeMetaAttribute = require('./scripts/mdx/rehype-meta-attribute')
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: 'vercel',
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  serverBuildTarget: 'netlify',
+  server: './server.js',
   ignoredRouteFiles: ['**/.*'],
-  ignoredRouteFiles: ['.*'],
   mdx: () => {
     return {
       rehypePlugins: [
