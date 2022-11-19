58e6d82f570fe9a3ea6957eb7a162914b0eb2687
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-11-19 12:37:17 +0000

    chore(demos): update CSS variables

--- demo/src/sandboxes/css-variables/src/App.tsx
+++ demo/src/sandboxes/css-variables/src/App.tsx
@@ -5,12 +5,14 @@ import styles from './styles.module.css'
 export default function App() {
   const [{ background }] = useSpring(
     () => ({
-      from: {
-        background: 'var(--from, pink)',
-      },
-      to: {
-        background: 'var(--to, purple)',
-      },
+      from: { background: 'var(--step0)' },
+      to: [
+        { background: 'var(--step0)' },
+        { background: 'var(--step1)' },
+        { background: 'var(--step2)' },
+        { background: 'var(--step3)' },
+        { background: 'var(--step4)' },
+      ],
       config: config.molasses,
       loop: {
         reverse: true,
@@ -21,7 +23,12 @@ export default function App() {
 
   return (
     <div className={styles.container}>
-      <animated.div className={styles.block} style={{ background }} />
+      <div className={styles.squares}>
+        <div className={styles.block} />
+        <div className={styles.block} />
+        <animated.div className={styles.block} style={{ background }} />
+      </div>
+      <animated.div className={styles.background} style={{ background }} />
     </div>
   )
 }
