70f33b754a4904ac019ea4b5a980b8273a8314cd
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-12-08 10:41:46 +0000

    docs: remove beta flags

--- docs/app/root.tsx
+++ docs/app/root.tsx
@@ -30,18 +30,18 @@ export const meta: MetaFunction = () => {
     'og:title': 'React Spring',
     'og:description':
       'Bring your components to life with simple spring animation primitives for React',
-    'og:image': 'https://beta.react-spring.dev/share.jpg',
+    'og:image': 'https://react-spring.dev/share.jpg',
     'og:type': 'website',
     'og:image:width': '1200',
     'og:image:height': '630',
-    'og:url': 'https://beta.react-spring.dev',
+    'og:url': 'https://react-spring.dev',
     'twitter:card': 'summary_large_image',
-    'twitter:domain': 'beta.react-spring.dev',
-    'twitter:url': 'https://beta.react-spring.dev',
+    'twitter:domain': 'react-spring.dev',
+    'twitter:url': 'https://react-spring.dev',
     'twitter:title': 'React Spring',
     'twitter:description':
       'Bring your components to life with simple spring animation primitives for React',
-    'twitter:image': 'https://beta.react-spring.dev/share.jpg',
+    'twitter:image': 'https://react-spring.dev/share.jpg',
   }
 }
 
