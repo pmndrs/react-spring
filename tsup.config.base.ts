8d93113639120a6db6336c16efc04f9f2c84632f
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-02-06 22:18:09 +0000

    chore(wip): use turbo

---
+++ tsup.config.base.ts
@@ -0,0 +1,24 @@
+import type { Format, Options } from 'tsup'
+
+export const defaultConfig = (
+  config: Omit<Options, 'external' | 'format' | 'minify' | 'dts'>,
+  options: Options
+) => ({
+  ...config,
+  clean: !options.watch,
+  target: 'esnext',
+  external: [
+    'react',
+    'react-dom',
+    'react-native',
+    '@react-three/fiber',
+    'three',
+    'react-konva',
+    'konva',
+    'react-zdog',
+    'zdog',
+  ],
+  dts: true,
+  minify: !options.watch,
+  format: ['cjs', 'esm'] as Format[],
+})
