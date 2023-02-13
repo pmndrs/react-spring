54dc1750d50394234ece5f135d25db84f34f7770
Florian Goße <florian@essog.de>
2023-02-13 20:14:04 +0100

    fix: allow container ref with null and undefined for useResize

--- docs/app/data/fixtures.tsx
+++ docs/app/data/fixtures.tsx
@@ -473,6 +473,14 @@ export const USE_SCROLL_CONFIG_DATA: CellData[][] = [
   ),
 ]
 
+export const USE_RESIZE_CONFIG_DATA: CellData[][] = [
+  ['container', 'React.MutableRefObject<HTMLElement | null | undefined>', null],
+  ...DEFAULT_CONFIG_DATA.filter(
+    row =>
+      row[0] !== 'from' && typeof row[0] === 'object' && row[0]?.label !== 'to'
+  ),
+]
+
 export const USESPRINGVALUE_CONFIG_DATA: CellData[][] = [
   [
     {
