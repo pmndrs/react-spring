d672c2014f889092adec4a84b740f1647bd9b159
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-02-25 11:37:36 +0000

    feat: add experimental release script

---
+++ scripts/version-and-publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Force start from root folder
+cd "$(dirname "$0")/.."
+
+set -e
+
+version=$VERSION
+distTag=$DIST_TAG
+
+if [[ -z "$version" ]]; then
+  echo "Please enter the version you want to publish"
+  read -r version
+fi
+
+if [[ -z "$distTag" ]]; then
+  echo "Please enter the dist-tag you want to publish with"
+  read -r distTag
+fi
+
+
+echo "$version"
+
+# publish packages
+./node_modules/.bin/changeset version --snapshot "$version"
+./node_modules/.bin/changeset publish --no-git-tag --snapshot --tag "$distTag"
