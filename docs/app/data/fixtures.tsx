eacdba9d2fe4bc820f4c5553d5667fc626c8646c
Joris W <joris@jor.is>
2025-02-09 11:58:39 -0300

    Clarify what `immediate` does

--- docs/app/data/fixtures.tsx
+++ docs/app/data/fixtures.tsx
@@ -517,7 +517,7 @@ export const USESPRINGVALUE_CONFIG_DATA: CellData[][] = [
   [
     {
       label: 'immediate',
-      content: <p>Prevents the animation if true.</p>,
+      content: <p>Prevents the animation if true, applying the `to` styles immediately.</p>,
     },
     {
       label: 'boolean | function',
