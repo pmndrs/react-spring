71d07a22efda2e8f146c1a2c6d76fdd360bbdde8
Robert Jarske Eriksson <jarske.eriksson@gmail.com>
2025-05-21 09:41:57 +0200

    add version check

--- packages/core/src/hooks/useTransition.tsx
+++ packages/core/src/hooks/useTransition.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { version as reactVersion } from 'react/package.json'
 import { useContext, useRef, useMemo } from 'react'
 import { Lookup, OneOrMore, UnknownProps } from '@react-spring/types'
 import {
@@ -436,6 +437,7 @@ export function useTransition(
           <elem.type
             {...elem.props}
             key={is.str(t.key) || is.num(t.key) ? t.key : t.ctrl.id}
+            ref={reactVersion < '19.0.0' ? elem.ref : elem.props.ref}
           />
         ) : (
           elem
