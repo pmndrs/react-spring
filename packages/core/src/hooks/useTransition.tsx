3a62f3ee8a869b7db5cb0082f2503c464af4c46c
Robert Jarske Eriksson <jarske.eriksson@gmail.com>
2025-05-24 19:58:02 +0200

    set props assignment to empty object if elem or elem.props is nullish

--- packages/core/src/hooks/useTransition.tsx
+++ packages/core/src/hooks/useTransition.tsx
@@ -436,9 +436,7 @@ export function useTransition(
         const key = is.str(t.key) || is.num(t.key) ? t.key : t.ctrl.id
         const isLegacyReact = React.version < '19.0.0'
 
-        const props = {
-          ...elem.props,
-        }
+        const props = elem?.props ?? {}
 
         if (isLegacyReact) {
           props.ref = elem.ref
