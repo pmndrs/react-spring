d99413c73c188c88a17e9a66311cb2408aacc292
Josh <joshua.ellis18@gmail.com>
2026-09-23 09:43:31 +0100

    fix(deps): update dependency cookie to v2

--- docs/app/helpers/theme.server.ts
+++ docs/app/helpers/theme.server.ts
@@ -1,15 +1,20 @@
-import * as cookie from 'cookie'
+import { parseCookie, stringifySetCookie } from 'cookie'
 
 const cookieName = 'en_theme'
 export type Theme = 'light' | 'dark'
 
 export function getTheme(request: Request): Theme | null {
   const cookieHeader = request.headers.get('cookie')
-  const parsed = cookieHeader ? cookie.parse(cookieHeader)[cookieName] : 'light'
+  const parsed = cookieHeader ? parseCookie(cookieHeader)[cookieName] : 'light'
   if (parsed === 'light' || parsed === 'dark') return parsed
   return null
 }
 
 export function setTheme(theme: Theme) {
-  return cookie.serialize(cookieName, theme, { path: '/', maxAge: 31536000 })
+  return stringifySetCookie({
+    name: cookieName,
+    value: theme,
+    path: '/',
+    maxAge: 31536000,
+  })
 }
